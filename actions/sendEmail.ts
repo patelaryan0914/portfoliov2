a860bfd9981655446892c3459ccbee7732d716f9
Aryan Patel <21cs038@charusat.edu.in>
2025-07-21 14:05:31 +0530

    small changes

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend("re_8tz53LqB_EMwyB1gZTrpbU1qGYjUz2fX6");
+const resend = new Resend(process.env.RESEND_API_KEY || "");
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
